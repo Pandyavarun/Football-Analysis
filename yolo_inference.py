59c0706c26ed80bd02363587314357b9ee583a49
Varun Pandya <varunpandya12a@gmail.com>
2024-10-18 22:03:42 +0530

    First Test

---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov8s')  # Load model
+
+results = model.predict('input_video/08fd33_4.mp4', save=True)  # Inference
+
+print(results[0])
+print('"""""""""""""""""""""""""""""""""""""')
+for box in results[0].boxes:
+    print(box)
