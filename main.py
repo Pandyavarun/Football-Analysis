e5789f01e28047f5136382ba0745895adefd9691
Varun Pandya <varunpandya12a@gmail.com>
2024-10-19 20:12:02 +0530

    Updated Tracker

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from utils import read_video, save_video
+from trackers import Tracker
+
+
 def main():
     # Read video
     video_frames = read_video('input_video/08fd33_4.mp4')
+
+    # Create tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracker(video_frames)
+
     # Save video
     save_video(video_frames, 'output_video/output.avi')
 
